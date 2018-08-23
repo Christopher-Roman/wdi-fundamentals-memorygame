e5decc6708aa68b9d17632b103d04d51dd97cbed
Chris Roman <Christopherroman85@gmail.com>
2018-08-23 14:44:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,22 +22,3 @@ var flipCard = function(cardID) {
 
 flipCard(0);
 flipCard(2);
-
-
-//var cardOne = cards[0];
-
-//var cardTwo = cards[1];
-
-//cardsInPlay.push(cardOne);
-//cardsInPlay.push(cardTwo);
-
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardTwo);
-
-//if (cardsInPlay.length === 2) {
-//	if (cardsInPlay[0] === cardsInPlay[1]) {
-//		alert("You found a match!");
-//	} else {
-//		alert("Sorry, try again.")
-//	}
-//}
