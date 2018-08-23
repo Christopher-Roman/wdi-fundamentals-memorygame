baba0df5f1ac51f8be884b052c2b9cf5a47a1971
Chris Roman <Christopherroman85@gmail.com>
2018-08-23 14:41:11 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,40 @@ var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.")
-	}
+  }
+ }
 }
+
+var flipCard = function(cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();	
+}
+
+flipCard(0);
+flipCard(2);
+
+
+//var cardOne = cards[0];
+
+//var cardTwo = cards[1];
+
+//cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardTwo);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+
+//if (cardsInPlay.length === 2) {
+//	if (cardsInPlay[0] === cardsInPlay[1]) {
+//		alert("You found a match!");
+//	} else {
+//		alert("Sorry, try again.")
+//	}
+//}
