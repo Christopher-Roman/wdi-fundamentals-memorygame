d1690b5dd89280f73e4fcc4f097a8dfe7427fdf9
Chris Roman <Christopherroman85@gmail.com>
2018-08-28 14:35:43 -0500

    complete game!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,4 @@
-console.log("Up and running!");
-
-var cardsInPlay = [];
-
+//List of available objects to match
 var cards = [
 {
 	rank: "queen",
@@ -24,25 +21,39 @@ var cards = [
 }
 ];
 
+//Empty Array to collect chosen cards
+var cardsInPlay = [];
+
+//Function to check for matches based on the cards that are flipped.
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.")
   }
  }
-}
 
-var flipCard = function(cardId) {
-  cardsInPlay.push(cards[cardId].rank);
-
-    console.log("User flipped " + cards[cardId].rank);
- 	console.log(cards[cardId].cardImage);
+//Function to flip cards, push cards to the empty array, and log object information to the console
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id')
+ 	cardsInPlay.push(cards[cardId].rank);
  	console.log(cards[cardId].suit); 
+ 	console.log(cards[cardId].cardImage);
+ 	this.setAttribute('src', cards[cardId].cardImage)
+//Call for the card check function if two cards are in play
+if (cardsInPlay.length === 2){
   checkForMatch();
- 
- }	
+ }
+};
 
-flipCard(0);
-flipCard(2);
+//Function to create the board
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+createBoard();
